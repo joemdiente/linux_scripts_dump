f43af818520e1a39e3c82a3593709db13c5e3506
Joem Diente <redphoenix741@yahoo.com.ph>
2023-06-21 15:30:47 +0300

    ims_proj: Easy snipe-it test deploy script.

---
+++ ims_proj/snipe_test_docker/deploy_test_snipe_it.sh
@@ -0,0 +1,4 @@
+sudo apt-get install docker.io
+sudo docker pull snipe/snipe-it:v6.1.0
+sudo docker run --name snipe-mysql --env-file=test_snipe_it_env --mount source=snipesql-vol,target=/var/lib/mysql -d -P mysql:5.6
+sudo docker run -d -p 8080:80 --name="snipeit" --link snipe-mysql:mysql --env-file=test_snipe_it_env --mount source=snipe-vol,dst=/var/lib/snipeit snipe/snipe-it:v6.1.0
