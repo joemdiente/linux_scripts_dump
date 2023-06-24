ef1ce565ccf10d52e1cece2583259f9c32894839
Joem Diente <redphoenix741@yahoo.com.ph>
2023-06-24 14:49:48 +0300

    ims_proj: eclipse settings + main.cpp. known working load ui.xml

--- ims_proj/eclipse_workspace/ims_gui/main.cpp
+++ ims_proj/eclipse_workspace/ims_gui/main.cpp
@@ -1,14 +1,55 @@
+#include <cxxopts.hpp>
 #include <egt/ui>
+#include <egt/uiloader.h>
 #include <iostream>
-using namespace std;
-using namespace egt;
 
-int main(int argc, const char **argv) {
-	Application app;
-	TopWindow window;
-	Button button(window, "Press Me");
-	center(button);
-	window.show();
+/*
+ * main Function
+ */
+int main(int argc, char** argv)
+{
+	std::cout << "Start of Program" << std::endl;
 
-	return app.run();
+    egt::Application app(argc, argv);
+#ifdef EXAMPLEDATA
+    egt::add_search_path(EXAMPLEDATA);
+#endif
+
+    cxxopts::Options options(argv[0], "load ui xml");
+    options.add_options()
+    ("h,help", "Show help")
+    ("i,input", "Input file", cxxopts::value<std::string>()->default_value("file:ui.xml"));
+    auto args = options.parse(argc, argv);
+    if (args.count("help"))
+    {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    egt::experimental::UiLoader loader;
+    auto window = loader.load(args["input"].as<std::string>());
+
+//    window->on_show([&]()
+//    {
+//        for (const auto& w : app.windows())
+//        {
+//#ifdef EGT_HAS_VIDEO
+//            if (w->type() == "VideoWindow")
+//            {
+//                egt::VideoWindow* vwin = dynamic_cast<egt::VideoWindow*>(w);
+//                vwin->play();
+//            }
+//#endif
+//#ifdef EGT_HAS_CAMERA
+//            if (w->type() == "CameraWindow")
+//            {
+//                egt::CameraWindow* cwin = dynamic_cast<egt::CameraWindow*>(w);
+//                cwin->start();
+//            }
+//#endif
+//        }
+//    });
+
+    window->show();
+    return app.run();
 }
