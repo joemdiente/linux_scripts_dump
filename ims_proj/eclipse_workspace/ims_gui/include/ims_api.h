0d61ee90052fb633f41d42c71a3b28e363f7e340
Joem Diente <redphoenix741@yahoo.com.ph>
2023-07-02 17:08:55 +0300

    ims_proj: added curl write callback, added return json object

--- ims_proj/eclipse_workspace/ims_gui/include/ims_api.h
+++ ims_proj/eclipse_workspace/ims_gui/include/ims_api.h
@@ -7,17 +7,26 @@
 
 #include <string>
 
+/* External Libraries */
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 class ims_api
 {
     private:
+        //Access Information
         std::string token;
         std::string url_base;
+
+        //HTTP Request
+        std::string header_json = "accept: application/json";
+        std::string header_auth = "Authorization: Bearer ";
 
     public:
         ims_api(const std::string t = "", 
             const std::string url = "");
-        std::string get_userdetails(void);
+        json get_userdetails(void);
 };
 
 
