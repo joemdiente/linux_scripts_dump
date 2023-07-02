c82577d516adcfaf2e3a368f93d9277afdf187f6
Joem Diente <redphoenix741@yahoo.com.ph>
2023-07-02 18:05:36 +0300

    ims_proj: working test code.

--- ims_proj/eclipse_workspace/ims_gui/main.cpp
+++ ims_proj/eclipse_workspace/ims_gui/main.cpp
@@ -11,6 +11,8 @@
 
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
+
+#define DEBUG_JSON() std::cout << j.dump(4) << std::endl
 /*
  * main program
  */
@@ -27,25 +29,32 @@ int main(int argc, char** argv)
 
 	auto button = window->find_child<egt::Button>("ok-1");
 
-	//Use ims_api
+	//Start of Test
+
+	//Use ims_api test
 	std::string test_url = "http://127.0.0.1:8080/api/v1";
-    std::string test_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxMCIsImp0aSI6IjUwZDU4MjYxN2Y3YjcxMGMxZmJhZmM4MDdkMGM1YjVkMGQyOGNiN2Q5YzYyYzNjMDQzNTFkZjljYmIzMDAwZTFhZGI2NGI3Njc0N2M0MDg4IiwiaWF0IjoxNjg3NjAxOTgzLjYwNzY2NSwibmJmIjoxNjg3NjAxOTgzLjYwNzY2OSwiZXhwIjoyMzE4NzU3NTgzLjU3Mzk1OSwic3ViIjoiMSIsInNjb3BlcyI6W119.snGBAFndn_J2XXC34js3A_yy5ZsEih7EpGgcCs3NKwVymrOB-ID3LbJnhQdxE2orNLtDRI8fyPEPSLKE9yAbT5E9ZV7vxcKjp4Jl_Ca8d4j30QbyZugU6errE3xZApVp6lddlxdxBUBREh_7_TV80zsy0Lp3BxCpHCPavAYukv4_7uE54nzC_vFAYDp-sqQcFhW6fIL-nUaoZoHZCsF0j0zWkvNxg0NX9ZZ1TmnsukS2r6fuliU85pkLMaxm6xD9XpFl0j6YdU_4jaxN-hQjz26pWm6RMh1NmBKC1xd8exW8q78huqCTqouqbIXAJDkLEZRXW5461hRHhklcWjB8WEoN8CIAzeGcqiw4QCL1FmBDCbSqmrMUpD7pIY6p4roDMzZDl1rWJ_L7JYDOGdMwnVLUdJG09Wfsj6it7t-6MEGl-Aj5Ld43cPH9IDJZ59H2WsVeOj3kHdM_3263AZlo4cNap7zR_AC0p_n8NeukihQh-oL8DzLYMLei9dntIBZxzJJsc4oS4JzpdLIey49-ajVzD1WHvFHlimBPFDvGVYPr0nceKIb4rLO0SHZei7DXrFwzxET0khoCPi0-CHSn1VAFmMf-4y1cR6njdqof1O9nf_DGpQkZsbZZsIc6vD7-aDjlToq9z7YciMTNxoZkg-kfIN13O8TeDB9YHLPmP34";
+    std::string test_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxMCIsImp0aSI6IjM3Y2NmZTZiMjZmNGE0OTE0ODhiM2Q1NmViYzVlYzA4YTAxNGM5MDZhMTBkM2EyZjUyMWVmZmE5ODM0MTE2MTkwNTk1NWVkZWJkZmJmYTE2IiwiaWF0IjoxNjg4MzA4MTE5Ljg5OTkxOCwibmJmIjoxNjg4MzA4MTE5Ljg5OTkyMSwiZXhwIjoyMzE5NDYzNzE5Ljg5NDA0OSwic3ViIjoiMjEzIiwic2NvcGVzIjpbXX0.EkmZ1nn0OkSKPkLx5Uqdh5-TXwXciq3n2EP-MM_rFOrYKUd4-fhFG2WO5rwhrzS4VTQt5iN4kDpPowlu3ihIJ4KeQH2wo5b_HBomvmjVaPXfpm0kyJi1qlgV5eyt_4_VrtVwZKO9B4URyprsEaBL8QL0WX_Js-wa2VNO345L6oYiMaU6upqlO8zC5DIPHIRR_AGON5fRpIt_946lNZ0Zj9ipydNBrlI2FHSJCYTIH8qsi2Ea39h8WUovAKApqwJoMvtIYP8Cm6cxDqOC4GpKBz_7C7yW2bQtcf21WG6HeV9qMXJCSjFvUN1HwDJRbjlMTFFkUUG7ah0SzbpXiAX8_qEymva6Rb_lPgnRPOu8eMvJAUKPnuBPhdg9gYOPyFpk9_pAVAjaAYyNhf_bTIqF0ARgGYHkPfMY3rJ8ErDFE694dJWnMzFK-HveJ5RTYd10DeeQyWB4aHVcgMqJdgZQSgSvyok4UqouB8x5Wys9RuMqflxopPnH6BUa1DxcmOd9JGqaKpzfxKrbe12vSPkYA-n778Yco6RdgmDBOgRHhBM0M8f2F6BjjLu6KiOe0vEztnkgk8ZpZCPhEpgiPnHUSZkFfHwoy4_OFepwDpTDjn2XKlyVebbGS6-cMkNGjQUnI-YxAnUqIaaY0ACIitLVrBB9_bZqKK1kQxks3l0uv_U";
 
 	ims_api ims_api_instance(test_token, test_url);
-
-	nlohmann::json j;
-
-	//Test
-	j = ims_api_instance.get_userdetails();
-	std::cout << j["email"] << std::endl;
-	std::cout << "end test" << std::endl;
-	return  0;
-
-	button->on_click([&label](egt::Event&)
+	
+	button->on_click([&label, &ims_api_instance](egt::Event&)
 	{
 		if (label->visible())
 		{
+			//Get User Details 
+			nlohmann::json j =ims_api_instance.get_userdetails();
+			j.dump(4);
+			DEBUG_JSON();
 			
+			//Format User Information
+			std::stringstream user_information;
+			user_information << \
+				"ID: " << j["id"] << " Badge: " << j["username"] << std::endl <<\
+				"E-Mail: " << j["email"] << std::endl <<\
+			std::endl;
+
+			label->text(user_information.str());	
+			label->default_text_align();		
 		}
 		else
 			label->visible(true);
