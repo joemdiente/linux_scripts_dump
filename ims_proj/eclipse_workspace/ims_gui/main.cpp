96e26200e62728cc336cfb4e1fa6d1b398199efd
Joem Diente <redphoenix741@yahoo.com.ph>
2023-07-02 17:10:11 +0300

    ims_proj: added curl writecallback, return should be JSON. let application get details from JSON. main.cpp test only.

--- ims_proj/eclipse_workspace/ims_gui/main.cpp
+++ ims_proj/eclipse_workspace/ims_gui/main.cpp
@@ -9,6 +9,8 @@
 // Inventory Management API Header
 #include "include/ims_api.h"
 
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 /*
  * main program
  */
@@ -19,18 +21,25 @@ int main(int argc, char** argv)
     egt::experimental::UiLoader loader;
     auto window = std::static_pointer_cast<egt::Window>(loader.load("file:ui.xml"));
 
-    std::cout << "test" << std::endl;
-
-    std::string url = "http://127.0.0.1:8080/api/v1";
-    std::string tok = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxMCIsImp0aSI6IjUwZDU4MjYxN2Y3YjcxMGMxZmJhZmM4MDdkMGM1YjVkMGQyOGNiN2Q5YzYyYzNjMDQzNTFkZjljYmIzMDAwZTFhZGI2NGI3Njc0N2M0MDg4IiwiaWF0IjoxNjg3NjAxOTgzLjYwNzY2NSwibmJmIjoxNjg3NjAxOTgzLjYwNzY2OSwiZXhwIjoyMzE4NzU3NTgzLjU3Mzk1OSwic3ViIjoiMSIsInNjb3BlcyI6W119.snGBAFndn_J2XXC34js3A_yy5ZsEih7EpGgcCs3NKwVymrOB-ID3LbJnhQdxE2orNLtDRI8fyPEPSLKE9yAbT5E9ZV7vxcKjp4Jl_Ca8d4j30QbyZugU6errE3xZApVp6lddlxdxBUBREh_7_TV80zsy0Lp3BxCpHCPavAYukv4_7uE54nzC_vFAYDp-sqQcFhW6fIL-nUaoZoHZCsF0j0zWkvNxg0NX9ZZ1TmnsukS2r6fuliU85pkLMaxm6xD9XpFl0j6YdU_4jaxN-hQjz26pWm6RMh1NmBKC1xd8exW8q78huqCTqouqbIXAJDkLEZRXW5461hRHhklcWjB8WEoN8CIAzeGcqiw4QCL1FmBDCbSqmrMUpD7pIY6p4roDMzZDl1rWJ_L7JYDOGdMwnVLUdJG09Wfsj6it7t-6MEGl-Aj5Ld43cPH9IDJZ59H2WsVeOj3kHdM_3263AZlo4cNap7zR_AC0p_n8NeukihQh-oL8DzLYMLei9dntIBZxzJJsc4oS4JzpdLIey49-ajVzD1WHvFHlimBPFDvGVYPr0nceKIb4rLO0SHZei7DXrFwzxET0khoCPi0-CHSn1VAFmMf-4y1cR6njdqof1O9nf_DGpQkZsbZZsIc6vD7-aDjlToq9z7YciMTNxoZkg-kfIN13O8TeDB9YHLPmP34";
-
-	ims_api api1(tok, url);
-	api1.get_userdetails();
-	return  0;
+    std::cout << "start test" << std::endl;
 
     auto label = window->find_child<egt::Label>("userdata-1");
 
 	auto button = window->find_child<egt::Button>("ok-1");
+
+	//Use ims_api
+	std::string test_url = "http://127.0.0.1:8080/api/v1";
+    std::string test_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxMCIsImp0aSI6IjUwZDU4MjYxN2Y3YjcxMGMxZmJhZmM4MDdkMGM1YjVkMGQyOGNiN2Q5YzYyYzNjMDQzNTFkZjljYmIzMDAwZTFhZGI2NGI3Njc0N2M0MDg4IiwiaWF0IjoxNjg3NjAxOTgzLjYwNzY2NSwibmJmIjoxNjg3NjAxOTgzLjYwNzY2OSwiZXhwIjoyMzE4NzU3NTgzLjU3Mzk1OSwic3ViIjoiMSIsInNjb3BlcyI6W119.snGBAFndn_J2XXC34js3A_yy5ZsEih7EpGgcCs3NKwVymrOB-ID3LbJnhQdxE2orNLtDRI8fyPEPSLKE9yAbT5E9ZV7vxcKjp4Jl_Ca8d4j30QbyZugU6errE3xZApVp6lddlxdxBUBREh_7_TV80zsy0Lp3BxCpHCPavAYukv4_7uE54nzC_vFAYDp-sqQcFhW6fIL-nUaoZoHZCsF0j0zWkvNxg0NX9ZZ1TmnsukS2r6fuliU85pkLMaxm6xD9XpFl0j6YdU_4jaxN-hQjz26pWm6RMh1NmBKC1xd8exW8q78huqCTqouqbIXAJDkLEZRXW5461hRHhklcWjB8WEoN8CIAzeGcqiw4QCL1FmBDCbSqmrMUpD7pIY6p4roDMzZDl1rWJ_L7JYDOGdMwnVLUdJG09Wfsj6it7t-6MEGl-Aj5Ld43cPH9IDJZ59H2WsVeOj3kHdM_3263AZlo4cNap7zR_AC0p_n8NeukihQh-oL8DzLYMLei9dntIBZxzJJsc4oS4JzpdLIey49-ajVzD1WHvFHlimBPFDvGVYPr0nceKIb4rLO0SHZei7DXrFwzxET0khoCPi0-CHSn1VAFmMf-4y1cR6njdqof1O9nf_DGpQkZsbZZsIc6vD7-aDjlToq9z7YciMTNxoZkg-kfIN13O8TeDB9YHLPmP34";
+
+	ims_api ims_api_instance(test_token, test_url);
+
+	nlohmann::json j;
+
+	//Test
+	j = ims_api_instance.get_userdetails();
+	std::cout << j["email"] << std::endl;
+	std::cout << "end test" << std::endl;
+	return  0;
 
 	button->on_click([&label](egt::Event&)
 	{
--- ims_proj/eclipse_workspace/ims_gui/src/ims_api.cpp
+++ ims_proj/eclipse_workspace/ims_gui/src/ims_api.cpp
@@ -10,14 +10,8 @@
 #include <string>
 #include <iostream>
 
-/* External Libraries */
-#include <curl/curl.h>
-#include <nlohmann/json.hpp>
-
 /* API Header */
 #include "../include/ims_api.h"
-std::string header_json = "accept: application/json";
-std::string header_auth = "Authorization: Bearer ";
 
 // Default Constructor
 ims_api::ims_api (const std::string t, const std::string url)
@@ -26,32 +20,56 @@ ims_api::ims_api (const std::string t, const std::string url)
     url_base = url;
 }
 
-std::string ims_api::get_userdetails(void)
+/* 
+ * CUrl Write Callback to a variable
+ * From: https://stackoverflow.com/questions/9786150/save-curl-content-result-into-a-string-in-c
+ */
+static size_t curl_wcb(void *contents, size_t size, size_t nmemb, void *userp)
+{ 
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+/*
+ * Get User Details of IMS API instance
+ * Returns a Nlohmann JSON Object
+ */
+json ims_api::get_userdetails(void)
 {
     std::string url_user_me = "/users/me";
     std::string header_auth_complete;
     std::string url_complete;
+    std::string curl_response;
+    json json_response;
 
     //Prepare Request
     url_complete = url_base + url_user_me;
     header_auth_complete = header_auth + token;
 
-    //Test Only
-    std::string json_response = "test_resp";
-    std::cout << header_auth_complete << std::endl;
-    std::cout << url_complete << std::endl;
-
     //Prepare CURL
-    CURL *hnd = curl_easy_init();
-    curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_easy_setopt(hnd, CURLOPT_WRITEDATA, stdout);
-    curl_easy_setopt(hnd, CURLOPT_URL, url_complete.c_str());
+    CURL *curl_handle = curl_easy_init();
+    curl_easy_setopt(curl_handle, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, curl_wcb);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &curl_response);
+    curl_easy_setopt(curl_handle, CURLOPT_URL, url_complete.c_str());
 
     struct curl_slist *headers = NULL;
     headers = curl_slist_append(headers, header_json.c_str());
     headers = curl_slist_append(headers, header_auth_complete.c_str());
-    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, headers);
 
-    CURLcode ret = curl_easy_perform(hnd);
+    CURLcode ret = curl_easy_perform(curl_handle);
+
+    if(curl_response.empty())
+    {
+        std::cout << "ERROR: No Response or Empty Response" << std::endl;
+        //Nothing to Parse
+        return json_response;
+    }
+
+    //Parse HTTP Response
+	json_response = json::parse(curl_response);
+
+    //Should only return required information TODO.
     return json_response;
 }
