ab98123bff649c39ef0ad4ef7152a40443b1a8a0
joemdiente <joemdiente@gmail.com>
2023-11-07 22:13:22 +0800

    app_testing: sample helloworld code for learning purposes.

---
+++ app_testing/helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!\r\n");
+   return 0;
+}
+
+
+/* Notes on testing:
+    gcc helloworld.c -o helloworld ;; normal build
+    gcc helloworld -g -o helloworld_debug ;; debug build
+*/
