9038be031c2fdbd563860bf18289808d934546ec
Joemel John A Diente <joemdiente@gmail.com>
2024-11-04 16:14:18 +0800

    igmp_test: setup route. required.

--- igmp_easy_setup.sh
+++ igmp_easy_setup.sh
@@ -6,6 +6,8 @@ echo "Make sure to run script as sudo else nothing will show below."
 sysctl net.ipv4.icmp_echo_ignore_broadcasts
 sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
 sysctl net.ipv4.icmp_echo_ignore_broadcasts
+
+ip route add 232.0.0.0/8 dev $IF
 #Join to multicast address
 # ip addr add 232.100.100.50/24 dev $IF autojoin
 # echo "Verify Below"
