bbfb93875fa97637328bbf725f02067c147bbbd7
Joemel John A Diente <joemdiente@gmail.com>
2024-11-04 16:13:52 +0800

    igmp_test: remove autojoin. creating interface only requires if VLC is not used. If VLC is used, no need to setup interface.

--- igmp_easy_setup.sh
+++ igmp_easy_setup.sh
@@ -7,6 +7,6 @@ sysctl net.ipv4.icmp_echo_ignore_broadcasts
 sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
 sysctl net.ipv4.icmp_echo_ignore_broadcasts
 #Join to multicast address
-ip addr add 232.0.1.50/24 dev $IF autojoin
-echo "Verify Below"
-ip -f inet maddr show dev $IF
+# ip addr add 232.100.100.50/24 dev $IF autojoin
+# echo "Verify Below"
+# ip -f inet maddr show dev $IF
