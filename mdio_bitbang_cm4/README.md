0bb0849cdd1452bbbca985e166f0cdf183e56ab2
Joem Diente <joemdiente@gmail.com>
2025-01-15 17:20:05 +0800

    mdio_bitbang_cm4: working device tree overlay with waveform capture and terminal output.

--- mdio_bitbang_cm4/README.md
+++ mdio_bitbang_cm4/README.md
@@ -8,3 +8,6 @@ https://github.com/torvalds/linux/blob/master/drivers/net/mdio/mdio-gpio.c
 
 loading dtoverlays on rpi
 https://forums.raspberrypi.com/viewtopic.php?t=330088
+
+decompile current device-tree 
+dtc -I fs -O dts /sys/firmware/devicetree/base > current.dts
--- mdio_bitbang_cm4/build_dtoverlay.sh
+++ mdio_bitbang_cm4/build_dtoverlay.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+echo "[Script Prompt] Cleaning up previous build...."
+sudo rm -rf ./*.dtbo
 echo "[Script Prompt] Running DTC...."
-sudo dtc -@ -I dts -O dtb -o mdio-gpio-cm4.dtbo mdio_gpio_cm4.dtso
-echo "[Script Prompt] Copying mdio-gpio-cm4.dtbo to /boot/overlays ..."
-sudo rsync -vrP ./mdio-gpio-cm4.dtbo /boot/overlays
+sudo dtc -@ -I dts -O dtb -o mdio-gpio-cm4.dtbo mdio-gpio-cm4.dtso
+sudo dtc -@ -I dts -O dtb -o spi1-3cs-overlay-test.dtbo spi1-3cs-overlay-test.dtso #For testing only
+echo "[Script Prompt] Copying *.dtbo to /boot/overlays and /boot/firmware/overlays ..."
+sudo rsync -vrP ./*.dtbo /boot/overlays
+sudo rsync -vrP ./*.dtbo /boot/firmware/overlays
 echo "[Script Prompt] Finished!"
 echo "[Script Prompt] ======================================================="
-echo "[Script Prompt] Make sure to add \"dtoverlay=mdio-gpio-cm4\" to /boot/firmware/config.txt (in CM4) then REBOOT"
+echo "[Script Prompt] Make sure to add \"dtoverlay=mdio-gpio-cm4\" to /boot/firmware/config.txt (in all) then REBOOT"
 echo "[Script Prompt] Tip: Line can be added under [cm4] or [all]...."
 echo "[Script Prompt] ======================================================+"
