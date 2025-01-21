8477a036b4868347f774b2b7fed12f24c25dfe06
Joem Diente <joemdiente@gmail.com>
2025-01-21 21:57:06 +0800

    mepa_mdio_tools: get string from output then convert it to hex uint16_t then store the value.

--- mdio_bitbang_cm4/mepa_mdio-tool_connector.c
+++ mdio_bitbang_cm4/mepa_mdio-tool_connector.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define STANDALONE_TEST
 
@@ -29,24 +30,77 @@ int mt_miim_write (uint8_t addr, uint16_t value)
 
 #ifdef STANDALONE_TEST
 
+//From gemini.google.com
+uint16_t hex_string_to_uint16(const char* hex_str) {
+    // Remove the "0x" prefix if present
+    char* stripped_str = strdup(hex_str); 
+    if (stripped_str[0] == '0' && stripped_str[1] == 'x') {
+        memmove(stripped_str, stripped_str + 2, strlen(stripped_str) - 1); 
+    }
+
+    if (strlen(stripped_str) != 4) {
+        fprintf(stderr, "Error: Invalid hex string length. Expected 4 characters.\n");
+        free(stripped_str); 
+        return 0; 
+    }
+
+    char high_byte[3] = {stripped_str[0], stripped_str[1], '\0'};
+    char low_byte[3] = {stripped_str[2], stripped_str[3], '\0'};
+
+    uint8_t high_byte_val = (uint8_t)strtol(high_byte, NULL, 16);
+    uint8_t low_byte_val = (uint8_t)strtol(low_byte, NULL, 16);
+
+    free(stripped_str); 
+
+    return (uint16_t)((high_byte_val << 8) | low_byte_val);
+}
+
 int main () {
 
-    char *buffer; //Buffer for storing shell output
-    size_t len;
-    int rc;
-    FILE *pipe = popen("sudo mdio -h", "r");
+    char output[1024]; //Buffer for storing shell output
+    char command[2048];
 
+    uint8_t phy_addr;
+    uint16_t reg_val;
+    size_t len;
+
+    int rc;
+
+    printf("Starting STANDALONE_TEST\r\n");
+
+    //Format command
+    phy_addr = 1;
+    reg_val = 2;
+    snprintf(command, sizeof(command), "sudo mdio gpio-0 phy %i raw %i", phy_addr, reg_val);
+
+    //Debug
+    printf("[DEBUG] %s \r\n",command);
+
+    //Open pipe
+    FILE *pipe = popen(command,"r");
+
+    //Check if opening popen failed
     if (pipe == NULL) {
         perror("popen() failed");
         return -1;
     }
-    
-    while ((rc = getline(&buffer, &len, pipe)) != -1) {
-        printf("%s", buffer); 
+
+    while (fgets(output, sizeof(output), pipe) != NULL) {
+        // Process the output as needed (e.g., print it)
+        //Debug
+        printf("[DEBUG] %s", output); 
     }
 
-    // output[strcspn(output,"\n")] = 0;
+    /* Expected output string is 0x0000
+     * Copy string to up to 6th char and then set 7th to \n (null ptr) 
+     */
+    char *reg_val_str;
+    output[6] = '\0';
+    reg_val = hex_string_to_uint16(output);
+    printf("[DEBUG] %s \r\n", output);
 
+    printf("[DEBUG] %X \r\n", reg_val);
+    
     pclose(pipe);
     return 0;
 }
