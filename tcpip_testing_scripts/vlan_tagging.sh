27cf988cb1acff5c9ccaf26fa910caa61956d9ad
Joemel John Diente <joemdiente@gmail.com>
2025-03-18 17:35:57 +0800

    tcpip_testing: added working qinq script; xstax_custom_modules: updated symlink.sh with comment only.

--- tcpip_testing_scripts/vlan_tagging.sh
+++ tcpip_testing_scripts/vlan_tagging.sh
@@ -10,16 +10,31 @@ TEST_MAC2=00:01:C1:00:00:02  #Vitesse
 TEST_MAC3=E0:BF:B2:00:00:02
 
 #Test IPs
-TEST_IP1=192.168.137.247
-TEST_IP2=192.168.137.2
+TEST_IP1=192.168.137.150
+TEST_IP2=192.168.137.25
 TEST_IP3=30.30.30.30
 TEST_IP4=1.1.1.1
 
-#Send a frame with C-tag (One Tag)
-for x in {1..2}
+#Generic 
+BC_MAC=ff:ff:ff:ff:ff:ff
+
+#Send a frame with S-TAG and C-TAG
+for x in {1..7}
 do
-    sudo ef tx $SELF_NET1 eth dmac ff:ff:ff:ff:ff:ff smac $TEST_MAC1 et 0x8100 \
-    ctag pcp 1 dei 0 vid 15 \
-    data ascii "hello world"
-    sleep 1
+    sudo ef tx $SELF_NET1 eth dmac $TEST_MAC2 smac $TEST_MAC1 \
+    et 0x88A8 stag pcp 0 dei 0 vid 7 \
+    et 0x8100 ctag pcp $x dei 0 vid 6 \
+    ipv4 sip $TEST_IP1 dip $TEST_IP2 proto 6 \
+    data ascii "aa55hello world"
+    # sleep 1
 done
+
+for x in {1..7}
+do
+    sudo ef tx $SELF_NET1 eth dmac ff:ff:ff:ff:ff:ff smac $TEST_MAC1 \
+    et 0x8100 ctag pcp $x dei 0 vid 9 \
+    ipv4 sip $TEST_IP1 dip $TEST_IP2 proto 6 \
+    data ascii "aa55hello world"
+    # sleep 1
+done
+
--- xstax_custom_modules/symlink.sh
+++ xstax_custom_modules/symlink.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# add in config.mk
+# Custom/AddModules := example_mod
 echo "make sure to run in istax src/ folder!!!!"
 ln -s ./module_example_mod.in ./build/make/
 ln -s ./example_mod ./vtss_appl/
