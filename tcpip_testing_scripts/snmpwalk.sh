827c09c1620893d959f3f168c6147fde8eab447d
Joemel John Diente <joemdiente@gmail.com>
2025-05-11 11:28:23 +0000

    tcpip_testing_scripts: updated snmpwalk.sh to include other settings.

--- tcpip_testing_scripts/snmpwalk.sh
+++ tcpip_testing_scripts/snmpwalk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SWITCHIP=192.168.137.116
+SWITCHIP=192.168.137.22
 
 ### Step 2:
 snmpwalk -v2c -c private $SWITCHIP VTSS-IP-MIB::vtssIpConfigInterfacesIpv4Active 
@@ -8,7 +8,10 @@ snmpwalk -v2c -c private $SWITCHIP VTSS-IP-MIB::vtssIpConfigInterfacesIpv4Ipv4Ad
 snmpwalk -v2c -c private $SWITCHIP VTSS-IP-MIB::vtssIpConfigInterfacesIpv4PrefixSize
 
 ### Step 3:
-snmpset -v2c -c private $SWITCHIP VTSS-IP-MIB::vtssIpConfigInterfacesIpv4Active.2 i 1 VTSS-IP-MIB::vtssIpConfigInterfacesIpv4PrefixSize.2 u 24 VTSS-IP-MIB::vtssIpConfigInterfacesIpv4EnableDhcpClient.2 i 1
+snmpset -v2c -c private $SWITCHIP VTSS-IP-MIB::vtssIpConfigInterfacesIpv4Active.2 i 1 \
+VTSS-IP-MIB::vtssIpConfigInterfacesIpv4PrefixSize.2 u 24 \
+VTSS-IP-MIB::vtssIpConfigInterfacesIpv4DhcpClientFallbackTimeout.2 u 0 \
+VTSS-IP-MIB::vtssIpConfigInterfacesIpv4EnableDhcpClient.2 i 1
 
 ### Step 4:
 snmpwalk -v2c -c private $SWITCHIP VTSS-IP-MIB::vtssIpConfigInterfacesIpv4Active 
